8b0d7cc20a5a5ed7c6fc9593a9a696d6d4b39f60
srinima <srinima@coursera.com>
2016-12-19 23:32:14 +0000

    'module1'

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,30 +1,33 @@
-(function () {
-'use strict';
+// (function () {
+// 'use strict';
+//
+//   angular.module('LunchCheck', [])
+//   .controller('LunchCheckController', LunchCheckController);
+//
+//   LunchCheckController.$inject = ['$scope'];
+//
+//   function LunchCheckController($scope) {
+//     $scope.lunchmenu="";
+//
+//     $scope.checkIfTooMuch = function () {
+//
+//
+//       var numMeals = $scope.lunchmenu.split(",");
+//       console.log(numMeals);
+//       if ($scope.lunchmenu == "") {
+//         $scope.message="Please enter data first";
+//       } else if (numMeals.length <= 3) {
+//         $scope.message="Enjoy";
+//       } else  {
+//         $scope.message="Too much";
+//       }
+//
+//     };
+//
+//   }
+//
+//
+// })();
 
-  angular.module('LunchCheck', [])
-  .controller('LunchCheckController', LunchCheckController);
 
-  LunchCheckController.$inject = ['$scope'];
-
-  function LunchCheckController($scope) {
-    $scope.lunchmenu="";
-
-    $scope.checkIfTooMuch = function () {
-
-
-      var numMeals = $scope.lunchmenu.split(",");
-      console.log(numMeals);
-      if ($scope.lunchmenu == "") {
-        $scope.message="Please enter data first";
-      } else if (numMeals.length <= 3) {
-        $scope.message="Enjoy";
-      } else  {
-        $scope.message="Too much";
-      }
-
-    };
-
-  }
-
-
-})();
+!function(){"use strict";function e(e){e.lunchmenu="",e.checkIfTooMuch=function(){var n=e.lunchmenu.split(",");""==e.lunchmenu?e.message="Please enter data first":n.length<=3?e.message="Enjoy":e.message="Too much"}}angular.module("LunchCheck",[]).controller("LunchCheckController",e),e.$inject=["$scope"]}();
